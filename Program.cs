c7c4b65b9716ac78489b42ea44b63823b59cfcba
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-19 22:27:23 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -71,9 +71,75 @@
 // строки с наименьшей суммой элементов: 1 строка
 
 
+// int [,] CreateArray()
+// {
+//     int [,] matr = new int [4,4];
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,11);
+//         }
+//     }
+//     return matr;
+// }
+
+// void PrintArray(int[,] matr)
+// {
+    
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write(matr[i,j]+" ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void SumRow (int[,] matr)
+// {
+// int sum = int.MaxValue;
+// int row = 0;
+
+//   for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         int temp = 0;
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             temp += matr[i,j];
+             
+//         }
+//         if (temp < sum)
+//             {
+//                sum = temp;
+//                row = i; 
+//             }
+//     }
+//     Console.WriteLine();
+//     Console.WriteLine("Сумма " + row + " строки наименьшая " + sum);
+// }
+
+// //************************************
+
+// int [,] matr = CreateArray();
+// System.Console.WriteLine("Задан массив:");
+// PrintArray(matr);
+// SumRow(matr);
+
+
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
 int [,] CreateArray()
 {
-    int [,] matr = new int [4,4];
+    int [,] matr = new int [2,2];
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
@@ -97,32 +163,26 @@ void PrintArray(int[,] matr)
     }
 }
 
-void SumRow (int[,] matr)
+void Multi (int[,] matr, int[,] array)
 {
-int sum = int.MaxValue;
-int row = 0;
-
-  for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        int temp = 0;
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            temp += matr[i,j];
-             
+            Console.Write("{0,2} ", matr[i, j] * array[i, j]);
         }
-        if (temp < sum)
-            {
-               sum = temp;
-               row = i; 
-            }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-    Console.WriteLine("Сумма " + row + " строки наименьшая " + sum);
 }
 
 //************************************
 
 int [,] matr = CreateArray();
-System.Console.WriteLine("Задан массив:");
+Console.WriteLine("Задан массив 1:");
 PrintArray(matr);
-SumRow(matr);
+int [,] array = CreateArray();
+Console.WriteLine();
+Console.WriteLine("Задан массив 2:");
+PrintArray(array);
+Console.WriteLine();
+Multi(matr, array);
