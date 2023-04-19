4cb343b87916eddd82c50f411ee4976ad37690ce
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-19 14:25:38 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+int [,] CreateArray()
+{
+    int [,] matr = new int [4,4];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,11);
+        }
+    }
+    return matr;
+}
+
+void PrintArray(int[,] matr)
+{
+    
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void rotation (int [,] newArray)
+{
+int[] temp = new int[newArray.GetLength(0)];
+ 
+Console.WriteLine("\nСортировка по строкам: ");
+for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            temp[j] = newArray[i, j];
+        Array.Sort(temp);
+        for (int k = 0; k < newArray.GetLength(0); k++)
+            {
+                newArray[i, k] = temp[k];
+                Console.Write("{0}\t", newArray[i, k]);
+            }
+        Console.WriteLine();
+    }
+}
+
+int [,] matr = CreateArray();
+System.Console.WriteLine("Задан массив:");
+PrintArray(matr);
+
+int [,] newArray = matr;
+
+rotation(newArray);
