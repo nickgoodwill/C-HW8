7fd1281b88507e1b2eb0c00fb90895555183dbe6
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-20 00:01:58 +0300

    forth task

--- Program.cs
+++ Program.cs
@@ -137,52 +137,98 @@
 // 15 18
 
 
-int [,] CreateArray()
+// int [,] CreateArray()
+// {
+//     int [,] matr = new int [2,2];
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,11);
+//         }
+//     }
+//     return matr;
+// }
+
+// void PrintArray(int[,] matr)
+// {
+    
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             System.Console.Write(matr[i,j]+" ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// void Multi (int[,] matr, int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write("{0,2} ", matr[i, j] * array[i, j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// //************************************
+
+// int [,] matr = CreateArray();
+// Console.WriteLine("Задан массив 1:");
+// PrintArray(matr);
+// int [,] array = CreateArray();
+// Console.WriteLine();
+// Console.WriteLine("Задан массив 2:");
+// PrintArray(array);
+// Console.WriteLine();
+// Multi(matr, array);
+
+// Сформируйте трёхмерный массив из случайных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int [,,] CreateArray()
 {
-    int [,] matr = new int [2,2];
+    int [,,] matr = new int [2,2,2];
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i,j] = new Random().Next(1,11);
+            for (int k = 0; k < matr.GetLength(2); k++)
+                {
+                    matr[i,j,k] = new Random().Next(1,11);
+                }
         }
+
     }
     return matr;
 }
 
-void PrintArray(int[,] matr)
+void PrintArray(int[,,] matr)
 {
     
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            System.Console.Write(matr[i,j]+" ");
+           for (int k = 0; k < matr.GetLength(2); k++) 
+                {
+                    Console.Write($" {matr[i, j, k]} ({i},{j},{k})");
+                }
+            Console.WriteLine();
         }
-        System.Console.WriteLine();
+
     }
 }
 
-void Multi (int[,] matr, int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write("{0,2} ", matr[i, j] * array[i, j]);
-        }
-        Console.WriteLine();
-    }
-}
-
-//************************************
-
-int [,] matr = CreateArray();
-Console.WriteLine("Задан массив 1:");
-PrintArray(matr);
-int [,] array = CreateArray();
-Console.WriteLine();
-Console.WriteLine("Задан массив 2:");
-PrintArray(array);
-Console.WriteLine();
-Multi(matr, array);
+ int [,,] matr = CreateArray();
+ Console.WriteLine("Задан массив:");
+ PrintArray(matr);
