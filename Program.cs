1633c7176f4a49520bf2a21bdc401c7a0ac7d3f4
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-21 14:07:15 +0300

    uncommit

--- Program.cs
+++ Program.cs
@@ -1,58 +1,58 @@
 ﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
 // по убыванию элементы каждой строки двумерного массива.
 
-// int [,] CreateArray()
-// {
-//     int [,] matr = new int [4,4];
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i,j] = new Random().Next(1,11);
-//         }
-//     }
-//     return matr;
-// }
+int [,] CreateArray()
+{
+    int [,] matr = new int [4,4];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,11);
+        }
+    }
+    return matr;
+}
 
-// void PrintArray(int[,] matr)
-// {
+void PrintArray(int[,] matr)
+{
     
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             System.Console.Write(matr[i,j]+" ");
-//         }
-//         System.Console.WriteLine();
-//     }
-// }
-// void rotation (int [,] newArray)
-// {
-// int[] temp = new int[newArray.GetLength(0)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void rotation (int [,] newArray)
+{
+int[] temp = new int[newArray.GetLength(0)];
  
-// Console.WriteLine("\nСортировка по строкам: ");
-// for (int i = 0; i < newArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < newArray.GetLength(1); j++)
-//             temp[j] = newArray[i, j];
-//         Array.Sort(temp);
-//         Array.Reverse(temp);
-//         for (int k = 0; k < newArray.GetLength(0); k++)
-//             {
-//                 newArray[i, k] = temp[k];
-//                 Console.Write("{0}\t", newArray[i, k]);
-//             }
-//         Console.WriteLine();
-//     }
-// }
+Console.WriteLine("\nСортировка по строкам: ");
+for (int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            temp[j] = newArray[i, j];
+        Array.Sort(temp);
+        Array.Reverse(temp);
+        for (int k = 0; k < newArray.GetLength(0); k++)
+            {
+                newArray[i, k] = temp[k];
+                Console.Write("{0}\t", newArray[i, k]);
+            }
+        Console.WriteLine();
+    }
+}
 
-// int [,] matr = CreateArray();
-// System.Console.WriteLine("Задан массив:");
-// PrintArray(matr);
+int [,] matr = CreateArray();
+System.Console.WriteLine("Задан массив:");
+PrintArray(matr);
 
-// int [,] newArray = matr;
+int [,] newArray = matr;
 
-// rotation(newArray);
+rotation(newArray);
 
 // Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить 
 // строку с наименьшей суммой элементов.
@@ -71,61 +71,61 @@
 // строки с наименьшей суммой элементов: 1 строка
 
 
-// int [,] CreateArray()
-// {
-//     int [,] matr = new int [4,4];
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i,j] = new Random().Next(1,11);
-//         }
-//     }
-//     return matr;
-// }
+int [,] CreateArray()
+{
+    int [,] matr = new int [4,4];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,11);
+        }
+    }
+    return matr;
+}
 
-// void PrintArray(int[,] matr)
-// {
+void PrintArray(int[,] matr)
+{
     
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             System.Console.Write(matr[i,j]+" ");
-//         }
-//         System.Console.WriteLine();
-//     }
-// }
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }
+}
 
-// void SumRow (int[,] matr)
-// {
-// int sum = int.MaxValue;
-// int row = 0;
+void SumRow (int[,] matr)
+{
+int sum = int.MaxValue;
+int row = 0;
 
-//   for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         int temp = 0;
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             temp += matr[i,j];
+  for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            temp += matr[i,j];
              
-//         }
-//         if (temp < sum)
-//             {
-//                sum = temp;
-//                row = i; 
-//             }
-//     }
-//     Console.WriteLine();
-//     Console.WriteLine("Сумма " + row + " строки наименьшая " + sum);
-// }
+        }
+        if (temp < sum)
+            {
+               sum = temp;
+               row = i; 
+            }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Сумма " + row + " строки наименьшая " + sum);
+}
 
 // //************************************
 
-// int [,] matr = CreateArray();
-// System.Console.WriteLine("Задан массив:");
-// PrintArray(matr);
-// SumRow(matr);
+int [,] matr = CreateArray();
+System.Console.WriteLine("Задан массив:");
+PrintArray(matr);
+SumRow(matr);
 
 
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
@@ -137,55 +137,55 @@
 // 15 18
 
 
-// int [,] CreateArray()
-// {
-//     int [,] matr = new int [2,2];
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             matr[i,j] = new Random().Next(1,11);
-//         }
-//     }
-//     return matr;
-// }
+int [,] CreateArray()
+{
+    int [,] matr = new int [2,2];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,11);
+        }
+    }
+    return matr;
+}
 
-// void PrintArray(int[,] matr)
-// {
+void PrintArray(int[,] matr)
+{
     
-//     for (int i = 0; i < matr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matr.GetLength(1); j++)
-//         {
-//             System.Console.Write(matr[i,j]+" ");
-//         }
-//         System.Console.WriteLine();
-//     }
-// }
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i,j]+" ");
+        }
+        System.Console.WriteLine();
+    }
+}
 
-// void Multi (int[,] matr, int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write("{0,2} ", matr[i, j] * array[i, j]);
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void Multi (int[,] matr, int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,2} ", matr[i, j] * array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
 
 // //************************************
 
-// int [,] matr = CreateArray();
-// Console.WriteLine("Задан массив 1:");
-// PrintArray(matr);
-// int [,] array = CreateArray();
-// Console.WriteLine();
-// Console.WriteLine("Задан массив 2:");
-// PrintArray(array);
-// Console.WriteLine();
-// Multi(matr, array);
+int [,] matr = CreateArray();
+Console.WriteLine("Задан массив 1:");
+PrintArray(matr);
+int [,] array = CreateArray();
+Console.WriteLine();
+Console.WriteLine("Задан массив 2:");
+PrintArray(array);
+Console.WriteLine();
+Multi(matr, array);
 
 // Сформируйте трёхмерный массив из случайных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
